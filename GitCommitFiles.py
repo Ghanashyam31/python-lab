2b857a9def347509a32c0ac49a0a82520b6c07a0
Ghanashyam31 <65525953+Ghanashyam31@users.noreply.github.com>
2020-07-02 14:37:07 +0530

    updated the Document1.txt

--- GitCommitFiles.py
+++ GitCommitFiles.py
@@ -2,9 +2,10 @@
 import sys,os
 from datetime import datetime
 DtTimestr=datetime.now().strftime('%d-%m-%Y-%H%M%S')
-msg1='Line no 10 added '+DtTimestr
+#msg1='Line no 10 added '+DtTimestr
+msg1=input("Enter the commit text")
 gtCmd='git commit -m "'+ msg1 +'"'
 #print('git commit -m "'+ msg1 +'"')
-os.system('git add Document1.txt')
+os.system('git add .')
 os.system(gtCmd)
 
